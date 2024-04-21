5b14e656cc132e47089d4a7ec815346ec653ef2f
ManuPLearner <87842979+ManuPLearner@users.noreply.github.com>
2024-04-21 23:26:17 +0530

    Updated

--- cypress/integration/3-own-work/FirstTest.js
+++ cypress/integration/3-own-work/FirstTest.js
@@ -1,1 +1,9 @@
 
+/// <reference types="cypress"/>
+describe('FirstTestSuite', () => {
+  
+      it('Verify the Positive TC', () => {
+        cy.visit('https://demo.nopcommerce.com/') 
+        cy.title().should('eq','nopCommerce demo store')
+      })
+  })
