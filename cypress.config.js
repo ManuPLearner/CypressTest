d9700459bf13efd2238650202f1c786fe050feaa
ManuParameshwarappa <manu.parameshwarappa@thomsonreuters.com>
2024-04-24 13:49:02 +0530

    buildspec cache15

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,13 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
+    testIsolation: false,
   },
 })
